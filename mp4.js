d2b0c3bbd6b21ed339fd34edf2fa85b08aaf1746
Gireesh1806 <124286012+Gireesh1806@users.noreply.github.com>
2023-09-10 21:38:26 +0530

    Add files via upload

---
+++ mp4.js
@@ -0,0 +1,33 @@
+let displayValue = "0";
+
+function appendToDisplay(value) {
+    if (displayValue === "0" && value !== ".") {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    document.getElementById("result").value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = "0";
+    document.getElementById("result").value = displayValue;
+}
+
+function deleteFromDisplay() {
+    displayValue = displayValue.slice(0, -1);
+    if (displayValue === "") {
+        displayValue = "0";
+    }
+    document.getElementById("result").value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+        document.getElementById("result").value = displayValue;
+    } catch (error) {
+        displayValue = "Error";
+        document.getElementById("result").value = displayValue;
+    }
+}
